f1ff04c580e66592262cbabdc1b09661b8c24028
Vladislav Melnik <GimliVlad@list.ru>
2025-02-02 18:21:16 +0300

    Add camera rotation

---
+++ Assets/Game/Scripts/GamePlay/GameObjects/Core/Components/Rotation/RotationComponent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Game.Core.Components
+{
+    public class RotationComponent : ITickable
+    {
+        private const string Axis = "Mouse X";
+        
+        private readonly Transform _transform;
+        private readonly float _sensitivity;
+        
+        private float _horizontalAngle;
+
+        public RotationComponent(Transform transform, float sensitivity)
+        {
+            _transform = transform;
+            _sensitivity = sensitivity;
+        }
+
+        public void Tick()
+        {
+            Rotate();
+        }
+
+        private void Rotate()
+        {
+            _horizontalAngle += _sensitivity * Input.GetAxis(Axis);
+
+            _transform.rotation = Quaternion.Euler(0, _horizontalAngle, 0);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/GamePlay/GameSystems/Controllers/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Game.Controllers
+{
+    public class CameraController : IInitializable, ILateTickable
+    {
+        private const string Axis = "Mouse Y";
+        
+        private readonly float _sensitivity;
+        private readonly float _verticalMinAngle;
+        private readonly float _verticalMaxAngle;
+        
+        private Transform _transform;
+        private float _verticalAngle;
+        private float _horizontalAngle;
+
+        public CameraController(float sensitivity, float verticalMinAngle, float verticalMaxAngle)
+        {
+            _sensitivity = sensitivity;
+            _verticalMinAngle = verticalMinAngle;
+            _verticalMaxAngle = verticalMaxAngle;
+        }
+
+        public void Initialize()
+        {
+            _transform = Camera.main.transform;
+            _verticalAngle = _transform.localEulerAngles.x;
+        }
+
+        public void LateTick()
+        {
+            Rotate();
+        }
+
+        private void Rotate()
+        {
+            _horizontalAngle = _transform.eulerAngles.y;
+            _verticalAngle -= Input.GetAxis(Axis) * _sensitivity;
+            _verticalAngle = Mathf.Clamp(_verticalAngle, _verticalMinAngle, _verticalMaxAngle);
+            
+            _transform.rotation = Quaternion.Euler(_verticalAngle, _horizontalAngle, 0);
+        }
+    }
+}
