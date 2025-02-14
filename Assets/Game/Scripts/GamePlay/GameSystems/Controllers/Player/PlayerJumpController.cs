fdcdb07992449f017b4287608bed7d836743f9d4
Vladislav Melnik <GimliVlad@list.ru>
2025-02-14 19:45:23 +0300

    Extra jump fix

--- Assets/Game/Scripts/GamePlay/GameSystems/Controllers/Player/PlayerJumpController.cs
+++ Assets/Game/Scripts/GamePlay/GameSystems/Controllers/Player/PlayerJumpController.cs
@@ -4,7 +4,7 @@ using Zenject;
 
 namespace Game.Controllers
 {
-    public class PlayerJumpController : ITickable
+    public class PlayerJumpController : ITickable, IFixedTickable
     {
         private const float MinMultiplier = 0.75f;
         private const float MaxMultiplier = 1f;
@@ -12,6 +12,7 @@ namespace Game.Controllers
         private readonly IJumper _player;
 
         private float _currentMultiplier;
+        private bool _isJumping;
 
         public PlayerJumpController(IJumper player)
         {
@@ -21,30 +22,30 @@ namespace Game.Controllers
         public void Tick()
         {
             if (Input.GetKeyDown(KeyCode.Space))
-                _player.Jump();
+            {
+                _isJumping = _player.Jump();
 
-            // if (Input.GetKeyDown(KeyCode.Space))
-            // {
-            //     if (_player.Jump())
-            //         _currentMultiplier = MinMultiplier;
-            //
-            //     return;
-            // }
-            //
-            // if (_currentMultiplier == 0)
-            //     return;
-            //
-            // if (Input.GetKey(KeyCode.Space))
-            //     AddExtraForce();
-            //
-            // if (Input.GetKeyUp(KeyCode.Space) || _currentMultiplier >= MaxMultiplier)
-            //     _currentMultiplier = 0;
+                if (_isJumping)
+                    _currentMultiplier = MinMultiplier;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space) || _currentMultiplier >= MaxMultiplier)
+            {
+                _isJumping = false;
+                _currentMultiplier = 0;
+            }
         }
 
-        // private void AddExtraForce()
-        // {
-        //     _currentMultiplier = Mathf.Clamp(_currentMultiplier + Time.deltaTime, MinMultiplier, MaxMultiplier);
-        //     _player.AddExtraForce(_currentMultiplier);
-        // }
+        public void FixedTick()
+        {
+            if (_isJumping)
+                AddExtraForce();
+        }
+
+        private void AddExtraForce()
+        {
+            _currentMultiplier = Mathf.Clamp(_currentMultiplier + Time.fixedDeltaTime, MinMultiplier, MaxMultiplier);
+            _player.AddExtraForce(_currentMultiplier);
+        }
     }
 }
