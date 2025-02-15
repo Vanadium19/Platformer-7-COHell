1c92a5250248e1d9207004987af43ebf50487a13
Vladislav Melnik <GimliVlad@list.ru>
2025-02-15 21:31:41 +0300

    Add test menu

---
+++ Assets/Game/Scripts/GamePlay/Menu/UI/MainMenuView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Game.Menu.Core;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Game.Menu.UI
+{
+    public class MainMenuView : MonoBehaviour
+    {
+        [SerializeField] private Button _playButton;
+        [SerializeField] private Button _exitButton;
+
+        private MenuFacade _menu;
+
+        [Inject]
+        private void Construct(MenuFacade menu)
+        {
+            _menu = menu;
+        }
+
+        private void OnEnable()
+        {
+            _playButton.onClick.AddListener(OnPlayClicked);
+            _exitButton.onClick.AddListener(OnExitClicked);
+        }
+
+        private void OnDisable()
+        {
+            _playButton.onClick.RemoveListener(OnPlayClicked);
+            _exitButton.onClick.RemoveListener(OnExitClicked);
+        }
+
+        private void OnPlayClicked()
+        {
+            _menu.LoadGame();
+        }
+
+        private void OnExitClicked()
+        {
+            _menu.ExitGame();
+        }
+    }
+}
