38c84c73ffb1985b01ebd624f5c4e3e9deab0c2e
Vladislav Melnik <GimliVlad@list.ru>
2025-02-18 11:55:24 +0300

    Add player sounds

---
+++ Assets/Game/Scripts/GamePlay/GameObjects/View/AnimatorStates/OneShotSound.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Game.View.AnimatorStates
+{
+    public class OneShotSound : StateMachineBehaviour
+    {
+        [Tooltip("Nullable")] [SerializeField] private AudioClip _enterClip;
+        [Tooltip("Nullable")] [SerializeField] private AudioClip _exitClip;
+
+        private AudioSource _audioSource;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (_audioSource == null)
+                _audioSource = animator.GetComponent<AudioSource>();
+
+            if (_enterClip != null)
+                _audioSource.PlayOneShot(_enterClip);
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (_exitClip != null)
+                _audioSource.PlayOneShot(_exitClip);
+        }
+    }
+}
